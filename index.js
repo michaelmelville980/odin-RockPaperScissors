a4ff33908f5090831fb4715ab4f989dead2f8e9f
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-04-18 10:23:35 -0400

    getHumanChoice function

--- index.js
+++ index.js
@@ -2,13 +2,30 @@
 // getComputerChoice Function
 const getComputerChoice = function() {
     let num = Math.random();
-    let choice = "rock";
+    let choice = "Rock";
     if (num > 0.333 && num < 0.666){
-        choice = "paper";
+        choice = "Paper";
     } else if (num > 0.666) {
-        choice = "scissors";
+        choice = "Scissors";
     }
     return choice;
 }
 
 console.log(getComputerChoice());
+
+
+
+
+// getHumanChoice Function (assumes user enters valid choice)
+const getHumanChoice = function() {
+    let response = prompt("Enter 'Rock', 'Paper', or 'Scissors'");
+    let output = "Rock";
+    if (response === "Paper") {
+        output = "Paper";
+    }else if (response === "Scissors"){
+        output = "Scissors";
+    }
+    return output;
+}
+
+console.log(getHumanChoice());
