22485483bf3b2162baa7c1b7d15799f26a7b02d0
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-04-18 10:29:46 -0400

    Making humanChoice case-insensitive and adding new function skeleton for step 4

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 // getComputerChoice Function
 const getComputerChoice = function() {
     let num = Math.random();
-    let choice = "Rock";
+    let choice = "rock";
     if (num > 0.333 && num < 0.666){
-        choice = "Paper";
+        choice = "paper";
     } else if (num > 0.666) {
-        choice = "Scissors";
+        choice = "scissors";
     }
     return choice;
 }
@@ -18,14 +18,25 @@ console.log(getComputerChoice());
 
 // getHumanChoice Function (assumes user enters valid choice)
 const getHumanChoice = function() {
-    let response = prompt("Enter 'Rock', 'Paper', or 'Scissors'");
-    let output = "Rock";
-    if (response === "Paper") {
-        output = "Paper";
-    }else if (response === "Scissors"){
-        output = "Scissors";
+    let response = prompt("Enter 'rock', 'paper', or 'scissors'").toLowerCase();
+    let output = "rock";
+    if (response === "paper") {
+        output = "paper";
+    }else if (response === "scissors"){
+        output = "scissors";
     }
     return output;
 }
 
 console.log(getHumanChoice());
+
+
+
+// Step 4: Declare the players score variables
+let humanScore = 0;
+let computerScore = 0;
+
+//playound function
+const playound = function(humanChoice, computerChoice){
+
+}
